2bce7cbb47e9953f519b2c62079d909ef77266f2
Brian <brianscooper1@gmail.com>
2016-03-23 22:13:51 -0400

    test commit

--- guess_numbers/program.py
+++ guess_numbers/program.py
@@ -8,15 +8,22 @@ print()
 
 the_number = random.randint(0,100)
 
+guess=-1
+name= input('What is your Name? ')
 
-guess_text = input('Guess a number between 0 and 100: ')
 
-guess =int(guess_text)
+print()
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
 
-if guess < the_number:
-    print('to low')
-elif guess > the_number:
-    print('too high')
-else:
-    print('winner')
+    guess = int(guess_text)
 
+    if guess < the_number:
+        print('{} your guess of {} was too low'.format(name,guess))
+    elif guess > the_number:
+        print('{} your guess of {} was too high'.format(name,guess))
+
+    else:
+        print('winner')
+
+print('done')
